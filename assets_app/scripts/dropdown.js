cdb1f9b1931c1166b80e2eec6de6c772047c68cc
4rfel <rafael_1999_@hotmail.com.br>
2018-11-24 16:33:51 -0200

    aa

--- assets_app/scripts/dropdown.js
+++ assets_app/scripts/dropdown.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.dropdown-trigger');
-  var instances = M.Dropdown.init(elems);
-  console.log(elems);
-  console.log(instances);
+  var instances = M.Dropdown.init(elems, autoClose=false);
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.datepicker');
+  var instances = M.Datepicker.init(elems);
+});
+
+console.log('9');
+
+document.addEventListener('DOMContentLoaded', function() {
+  var i = 1
+  console.log(i);
+  let test = document.querySelector('.click')
+  if test{
+    i += 1;
+    console.log(i);
+  }
 });
