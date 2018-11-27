52a47bcf153a2b9812c7fdf50170a9bceacda7dd
4rfel <38050598+4rfel@users.noreply.github.com>
2018-11-27 00:24:28 -0200

    aaa

--- assets_app/scripts/dropdown.js
+++ assets_app/scripts/dropdown.js
@@ -16,6 +16,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+var i = 1
+function evento(){
+  i += 1
+  console.log(i);
+  let questao = document.querySelector('.souburro')
+  questao.innerHTML = "Questão número: " + i
+
+}
 
 
 
@@ -44,3 +52,44 @@ $(document).ready(function ()
             closeOnClick: false,
         });
     });
+
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+        // let db = new DB('https://co-des.firebaseio.com')
+        let db = new DB('https://desgosto-final.firebaseio.com/')
+
+        let params = new URLSearchParams(window.location.search)
+
+        let url = '/'
+        // Printing the path on firebase on the console
+        console.log(url)
+        db.download(url, function(data) {
+            // Printing the data contained on that path on firebase
+            console.log(data)
+
+            if(migue == 1){
+              console.log('qq');
+                var migue = 0;
+                let resp1 = document.querySelector('.resp1')
+                resp1.innerHTML = data["pergunta "+(i)]['respostas'][0]
+                // console.log(data["pergunta "+(i)]['respostas'][0])
+
+                let resp2 = document.querySelector('.resp2')
+                resp2.innerHTML = data["pergunta "+(i)]['respostas'][1]
+                // console.log(data["pergunta "+(i)]['respostas'][1])
+
+                let resp3 = document.querySelector('.resp3')
+                resp3.innerHTML = data["pergunta "+(i)]['respostas'][2]
+                // console.log(data["pergunta "+(i)]['respostas'][2])
+
+                let resp4 = document.querySelector('.resp4')
+                resp4.innerHTML = data["pergunta "+(i)]['respostas'][3]
+                // console.log(data["pergunta "+(i)]['respostas'][3])
+
+                let pergunta = document.querySelector('.pergunta')
+                pergunta.innerHTML = data["pergunta "+(i)]['respostas'][0]
+                // console.log(data["pergunta "+(i)]['pergunta'])
+          }
+        })
+    })
