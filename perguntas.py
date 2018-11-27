01800ecda87a05222f4f09c82ad7bfb48b41250b
4rfel <rafael_1999_@hotmail.com.br>
2018-11-27 11:52:24 -0200

    a

--- perguntas.py
+++ perguntas.py
@@ -4,7 +4,7 @@ import numpy as np
 main = {}
 main['perguntas'] = {}
 for e in range(1,101):
-    main['perguntas'][str(e)] = {"pergunta":"pergunta muito séria sobre carros {0}".format(str(e)), 'respostas':["resposta muito mais séria que a pergunta " + str(e) + " n 1","resposta muito mais séria que a pergunta " + str(e) + " n 2","resposta muito mais séria que a pergunta " + str(e) + " n 3","resposta muito mais séria que a pergunta " + str(e) + " n 4"], 'resposta_correta': np.random.randint(1,5)}
+    main['perguntas'][str(e)] = {"pergunta":"pergunta muito séria sobre carros {0}".format(str(e)), 'respostas':["resposta muito mais séria do que a pergunta " + str(e) + " n 1","resposta muito mais séria do que a pergunta " + str(e) + " n 2","resposta muito mais séria do que a pergunta " + str(e) + " n 3","resposta muito mais séria do que a pergunta " + str(e) + " n 4"], 'resposta_correta': np.random.randint(1,5)}
 
 
 
